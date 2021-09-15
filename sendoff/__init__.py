5b3a6082ff7cf2296bfe5147531e0602b5232ed7
Yakov Pechersky <ffxen158@gmail.com>
2021-09-14 21:57:28 -0400

    black formatting and docstrings

--- sendoff/__init__.py
+++ sendoff/__init__.py
@@ -1,1 +1,7 @@
-__version__ = '0.1.0'
+"""Get metadata of SD Files in a lightweight way."""
+"""
+The `sendoff` library provides lightweight tools for inspecting the metadata
+of chemical SD Files.  It strives to do as little modification of the input
+files as possible.
+"""
+__version__ = "0.1.0"
---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+"""Tests for the `sendoff` library."""
+"""
+The `sendoff` library tests validate the expected function of the library.
+"""
