26775b81bdb1550a6da9f96d7eaa539ce84adf3a
Yakov Pechersky <ffxen158@gmail.com>
2021-09-15 10:51:31 -0400

    unfixture methane and ethane, in prep for oe code

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -4,54 +4,34 @@ from rdkit import Chem
 
 
 @pytest.fixture
-def methane():
-    """Provide a molecule with no metadata.
-
-    Returns:
-        Methane, as an rdkit.Chem.rdchem.Mol
-    """
-    return Chem.MolFromSmiles("C")
-
-
-@pytest.fixture
-def ethane():
-    """Provide a molecule with no metadata.
-
-    Returns:
-        Ethane, as an rdkit.Chem.rdchem.Mol
-    """
-    return Chem.MolFromSmiles("CC")
-
-
-@pytest.fixture
-def single_mol_sdf(methane, tmp_path):
+def single_mol_sdf(tmp_path):
     """Write a single molecule with no metadata to an sdf.
 
     Args:
-        methane: pytest fixture providing a single molecule
         tmp_path: pytest fixture for writing files to a temp directory
 
     Returns:
         Path to the sdf
     """
+    mol = Chem.MolFromSmiles("C")
     outpath = tmp_path / "input.sdf"
     with Chem.SDWriter(str(outpath)) as sdw:
-        sdw.write(methane)
+        sdw.write(mol)
     return outpath
 
 
 @pytest.fixture
-def double_mol_sdf(methane, ethane, tmp_path):
+def double_mol_sdf(tmp_path):
     """Write two molecules with no metadata to an sdf.
 
     Args:
-        methane: pytest fixture providing a single molecule
-        ethane: pytest fixture providing a single molecule
         tmp_path: pytest fixture for writing files to a temp directory
 
     Returns:
         Path to the sdf
     """
+    methane = Chem.MolFromSmiles("C")
+    ethane = Chem.MolFromSmiles("CC")
     outpath = tmp_path / "input.sdf"
     with Chem.SDWriter(str(outpath)) as sdw:
         sdw.write(methane)
@@ -60,17 +40,16 @@ def double_mol_sdf(methane, ethane, tmp_path):
 
 
 @pytest.fixture
-def single_titled_mol_sdf(methane, tmp_path):
+def single_titled_mol_sdf(tmp_path):
     """Write a single molecule with only title metadata to an sdf.
 
     Args:
-        methane: pytest fixture providing a single molecule
         tmp_path: pytest fixture for writing files to a temp directory
 
     Returns:
         Path to the sdf
     """
-    mol = methane
+    mol = Chem.MolFromSmiles("C")
     mol.SetProp("_Name", "Title")
     outpath = tmp_path / "input.sdf"
     with Chem.SDWriter(str(outpath)) as sdw:
@@ -79,17 +58,16 @@ def single_titled_mol_sdf(methane, tmp_path):
 
 
 @pytest.fixture
-def single_delimiter_titled_mol_sdf(methane, tmp_path):
+def single_delimiter_titled_mol_sdf(tmp_path):
     """Write a single molecule with delimiter title metadata to an sdf.
 
     Args:
-        methane: pytest fixture providing a single molecule
         tmp_path: pytest fixture for writing files to a temp directory
 
     Returns:
         Path to the sdf
     """
-    mol = methane
+    mol = Chem.MolFromSmiles("C")
     mol.SetProp("_Name", "$$$$")
     outpath = tmp_path / "input.sdf"
     with Chem.SDWriter(str(outpath)) as sdw:
@@ -98,17 +76,16 @@ def single_delimiter_titled_mol_sdf(methane, tmp_path):
 
 
 @pytest.fixture
-def single_record_mol_sdf(methane, tmp_path):
+def single_record_mol_sdf(tmp_path):
     """Write a single molecule with only record value metadata to an sdf.
 
     Args:
-        methane: pytest fixture providing a single molecule
         tmp_path: pytest fixture for writing files to a temp directory
 
     Returns:
         Path to the sdf
     """
-    mol = methane
+    mol = Chem.MolFromSmiles("C")
     mol.SetProp("Record", "Value")
     outpath = tmp_path / "input.sdf"
     with Chem.SDWriter(str(outpath)) as sdw:
@@ -117,17 +94,16 @@ def single_record_mol_sdf(methane, tmp_path):
 
 
 @pytest.fixture
-def single_delimiter_record_mol_sdf(methane, tmp_path):
+def single_delimiter_record_mol_sdf(tmp_path):
     """Write a single molecule with delimiter record value metadata to an sdf.
 
     Args:
-        methane: pytest fixture providing a single molecule
         tmp_path: pytest fixture for writing files to a temp directory
 
     Returns:
         Path to the sdf
     """
-    mol = methane
+    mol = Chem.MolFromSmiles("C")
     mol.SetProp("Record", "$$$$")
     outpath = tmp_path / "input.sdf"
     with Chem.SDWriter(str(outpath)) as sdw:
@@ -136,17 +112,16 @@ def single_delimiter_record_mol_sdf(methane, tmp_path):
 
 
 @pytest.fixture
-def single_multiline_record_mol_sdf(methane, tmp_path):
+def single_multiline_record_mol_sdf(tmp_path):
     """Write a single molecule with a multiline record value metadata to an sdf.
 
     Args:
-        methane: pytest fixture providing a single molecule
         tmp_path: pytest fixture for writing files to a temp directory
 
     Returns:
         Path to the sdf
     """
-    mol = methane
+    mol = Chem.MolFromSmiles("C")
     value = str.join("\n", map(str, [0, 1, 2, 3]))
     mol.SetProp("Record", value)
     outpath = tmp_path / "input.sdf"
@@ -156,17 +131,16 @@ def single_multiline_record_mol_sdf(methane, tmp_path):
 
 
 @pytest.fixture
-def single_empty_string_record_mol_sdf(methane, tmp_path):
+def single_empty_string_record_mol_sdf(tmp_path):
     """Write a single molecule with an empty string record value to an sdf.
 
     Args:
-        methane: pytest fixture providing a single molecule
         tmp_path: pytest fixture for writing files to a temp directory
 
     Returns:
         Path to the sdf
     """
-    mol = methane
+    mol = Chem.MolFromSmiles("C")
     mol.SetProp("Record", "")
     outpath = tmp_path / "input.sdf"
     with Chem.SDWriter(str(outpath)) as sdw:
