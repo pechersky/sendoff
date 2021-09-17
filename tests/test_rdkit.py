590285c2b84b52cb5edbb96508d398534e0fb5d8
Yakov Pechersky <ffxen158@gmail.com>
2021-09-17 13:58:24 -0400

    typing annot on tests, and more delim tests

--- tests/test_rdkit.py
+++ tests/test_rdkit.py
@@ -1,8 +1,10 @@
 """Test rdkit parsing of metadata on generated SDFs."""
 from rdkit import Chem
 
+from sendoff.sdblock import Pathy
 
-def test_single_mol_read(single_mol_sdf):
+
+def test_single_mol_read(single_mol_sdf: Pathy) -> None:
     """An sdf of a single molecule contains one molecule.
 
     Args:
@@ -12,7 +14,7 @@ def test_single_mol_read(single_mol_sdf):
     assert len(mols) == 1
 
 
-def test_double_mol_read(double_mol_sdf):
+def test_double_mol_read(double_mol_sdf: Pathy) -> None:
     """An sdf of two molecule contains two molecules.
 
     Args:
@@ -22,7 +24,7 @@ def test_double_mol_read(double_mol_sdf):
     assert len(mols) == 2
 
 
-def test_titled_mol_read(single_titled_mol_sdf):
+def test_titled_mol_read(single_titled_mol_sdf: Pathy) -> None:
     """An sdf of a single titled molecule contains it with the expected title.
 
     Args:
@@ -32,7 +34,9 @@ def test_titled_mol_read(single_titled_mol_sdf):
     assert mol.GetProp("_Name") == "Title"
 
 
-def test_delimiter_titled_mol_read(single_delimiter_titled_mol_sdf):
+def test_delimiter_titled_mol_read(
+    single_delimiter_titled_mol_sdf: Pathy,
+) -> None:
     """An sdf of a delimiter titled molecule contains it with the expected title.
 
     Args:
@@ -42,7 +46,37 @@ def test_delimiter_titled_mol_read(single_delimiter_titled_mol_sdf):
     assert mol.GetProp("_Name") == "$$$$"
 
 
-def test_single_record_mol_read(single_record_mol_sdf):
+def test_first_delimiter_titled_mol_read(
+    double_first_delimiter_titled_mol_sdf: Pathy,
+) -> None:
+    """SDF with 1st molecule delimiter titled, containing with expected titles.
+
+    Args:
+        double_first_delimiter_titled_mol_sdf: pytest fixture of a Path to sdf
+    """
+    supp = Chem.SDMolSupplier(str(double_first_delimiter_titled_mol_sdf))
+    mol = next(supp)
+    other_mol = next(supp)
+    assert mol.GetProp("_Name") == "$$$$"
+    assert other_mol.GetProp("_Name") == "Title"
+
+
+def test_second_delimiter_titled_mol_read(
+    double_second_delimiter_titled_mol_sdf: Pathy,
+) -> None:
+    """SDF with 2nd molecule delimiter titled, containing with expected titles.
+
+    Args:
+        double_second_delimiter_titled_mol_sdf: pytest fixture of a Path to sdf
+    """
+    supp = Chem.SDMolSupplier(str(double_second_delimiter_titled_mol_sdf))
+    other_mol = next(supp)
+    mol = next(supp)
+    assert other_mol.GetProp("_Name") == "Title"
+    assert mol.GetProp("_Name") == "$$$$"
+
+
+def test_single_record_mol_read(single_record_mol_sdf: Pathy) -> None:
     """An sdf of a delimiter record valued molecule contains the expected data.
 
     Args:
@@ -52,7 +86,9 @@ def test_single_record_mol_read(single_record_mol_sdf):
     assert mol.GetProp("Record") == "Value"
 
 
-def test_delimiter_record_mol_read(single_delimiter_record_mol_sdf):
+def test_delimiter_record_mol_read(
+    single_delimiter_record_mol_sdf: Pathy,
+) -> None:
     """An sdf of a delimiter record valued molecule contains the expected data.
 
     Args:
@@ -62,7 +98,9 @@ def test_delimiter_record_mol_read(single_delimiter_record_mol_sdf):
     assert mol.GetProp("Record") == "$$$$"
 
 
-def test_multiline_record_mol_read(single_multiline_record_mol_sdf):
+def test_multiline_record_mol_read(
+    single_multiline_record_mol_sdf: Pathy,
+) -> None:
     """An sdf of a multiline record valued molecule contains the expected data.
 
     Args:
@@ -72,7 +110,9 @@ def test_multiline_record_mol_read(single_multiline_record_mol_sdf):
     assert mol.GetProp("Record") == "0\n1\n2\n3"
 
 
-def test_empty_string_record_mol_read(single_empty_string_record_mol_sdf):
+def test_empty_string_record_mol_read(
+    single_empty_string_record_mol_sdf: Pathy,
+) -> None:
     """An sdf of an empty string record valued molecule contains the expected data.
 
     Args:
