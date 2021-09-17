f9a49671e4fc5ff33b05ce50ccba91b91799ad17
Yakov Pechersky <ffxen158@gmail.com>
2021-09-17 12:57:44 -0400

    Fixture typing

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,10 +1,12 @@
 """Provide fixtures of SDFs with well- or misbehaved metadata."""
+from pathlib import Path
+
 import pytest
 from rdkit import Chem
 
 
 @pytest.fixture
-def single_mol_sdf(tmp_path):
+def single_mol_sdf(tmp_path: Path) -> Path:
     """Write a single molecule with no metadata to an sdf.
 
     Args:
@@ -21,7 +23,7 @@ def single_mol_sdf(tmp_path):
 
 
 @pytest.fixture
-def double_mol_sdf(tmp_path):
+def double_mol_sdf(tmp_path: Path) -> Path:
     """Write two molecules with no metadata to an sdf.
 
     Args:
@@ -40,7 +42,7 @@ def double_mol_sdf(tmp_path):
 
 
 @pytest.fixture
-def single_titled_mol_sdf(tmp_path):
+def single_titled_mol_sdf(tmp_path: Path) -> Path:
     """Write a single molecule with only title metadata to an sdf.
 
     Args:
@@ -58,7 +60,7 @@ def single_titled_mol_sdf(tmp_path):
 
 
 @pytest.fixture
-def single_delimiter_titled_mol_sdf(tmp_path):
+def single_delimiter_titled_mol_sdf(tmp_path: Path) -> Path:
     """Write a single molecule with delimiter title metadata to an sdf.
 
     Args:
@@ -76,7 +78,49 @@ def single_delimiter_titled_mol_sdf(tmp_path):
 
 
 @pytest.fixture
-def single_record_mol_sdf(tmp_path):
+def double_first_delimiter_titled_mol_sdf(tmp_path: Path) -> Path:
+    """Write two molecules with the first as delimiter title metadata to an sdf.
+
+    Args:
+        tmp_path: pytest fixture for writing files to a temp directory
+
+    Returns:
+        Path to the sdf
+    """
+    mol = Chem.MolFromSmiles("C")
+    mol.SetProp("_Name", "$$$$")
+    other_mol = Chem.MolFromSmiles("CC")
+    other_mol.SetProp("_Name", "Title")
+    outpath = tmp_path / "input.sdf"
+    with Chem.SDWriter(str(outpath)) as sdw:
+        sdw.write(mol)
+        sdw.write(other_mol)
+    return outpath
+
+
+@pytest.fixture
+def double_second_delimiter_titled_mol_sdf(tmp_path: Path) -> Path:
+    """Write two molecules with the second as delimiter title metadata to an sdf.
+
+    Args:
+        tmp_path: pytest fixture for writing files to a temp directory
+
+    Returns:
+        Path to the sdf
+    """
+    other_mol = Chem.MolFromSmiles("CC")
+    other_mol.SetProp("_Name", "Title")
+    mol = Chem.MolFromSmiles("C")
+    mol.SetProp("_Name", "$$$$")
+    outpath = tmp_path / "input.sdf"
+    with Chem.SDWriter(str(outpath)) as sdw:
+        sdw.write(other_mol)
+        sdw.write(mol)
+    return outpath
+
+
+@pytest.fixture
+def single_record_mol_sdf(tmp_path: Path) -> Path:
     """Write a single molecule with only record value metadata to an sdf.
 
     Args:
@@ -94,7 +138,7 @@ def single_record_mol_sdf(tmp_path):
 
 
 @pytest.fixture
-def single_delimiter_record_mol_sdf(tmp_path):
+def single_delimiter_record_mol_sdf(tmp_path: Path) -> Path:
     """Write a single molecule with delimiter record value metadata to an sdf.
 
     Args:
@@ -112,7 +156,7 @@ def single_delimiter_record_mol_sdf(tmp_path):
 
 
 @pytest.fixture
-def single_multiline_record_mol_sdf(tmp_path):
+def single_multiline_record_mol_sdf(tmp_path: Path) -> Path:
     """Write a single molecule with a multiline record value metadata to an sdf.
 
     Args:
@@ -131,7 +175,7 @@ def single_multiline_record_mol_sdf(tmp_path):
 
 
 @pytest.fixture
-def single_empty_string_record_mol_sdf(tmp_path):
+def single_empty_string_record_mol_sdf(tmp_path: Path) -> Path:
     """Write a single molecule with an empty string record value to an sdf.
 
     Args:
